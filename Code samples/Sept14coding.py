08420275eee26aea697eabc8d0b1d47cb1f573bb
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2026-09-14 20:00:44 -0400

    Add files via upload

---
+++ Code samples/Sept14coding.py
@@ -0,0 +1,21 @@
+# in-class coding from Monday, September 14
+
+import math  #basic syntax
+
+# calculate the cube root of a number
+
+answer = math.cbrt(-27)
+print (answer)
+"""
+#without the math library
+answer = (-27)**(1/3)
+print (answer)
+"""
+
+
+print(math.cos(math.pi/2))
+
+
+
+
+
---
+++ Code samples/additional_0914coding.py
@@ -0,0 +1,13 @@
+#other ways to do this
+
+from math import sin, cos
+
+print("sin of 0 is ", sin(0))
+
+print("cosine of 0 is", cos(0))
+
+# i could do this
+
+import math as m
+
+print("tangent of pi is ", m.tan(m.pi))
